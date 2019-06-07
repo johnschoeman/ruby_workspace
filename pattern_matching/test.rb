0d4caff673c78e78256e370f6bf3247035ba9f20
John Schoeman <johnschoeman1617@gmail.com>
2019-06-07 11:28:06 -0400

    multiline?

--- pattern_matching/test.rb
+++ pattern_matching/test.rb
@@ -10,10 +10,38 @@ class ExtractData
   end
 
   def extract_data_as_hash
-    series.
-      map { |single_series| extract_tag_one_data(single_series) }.
-      map { |tag_data| [tag_data[:tag], tag_data] }.
-      to_h
+    case JSON.parse(@response, symbolize_names: true)
+    in {
+      results: [
+        {
+          statement_id: id,
+          series: [
+            { 
+              name: series_one_name,
+              tags: { tag_name: tag_one },
+              values: [*rest_one, [_, value_one]]
+            },
+            {
+              name: series_two_name,
+              tags: { tag_name: tag_two },
+              values: [*rest_two, [_, value_two]]
+            }
+          ]
+        }
+      ]
+    }
+
+    {
+      "#{tag_one}": {
+      tag: tag_one,
+      count: value_one,
+    },
+      "#{tag_two}": {
+      tag: tag_two,
+      count: value_two
+      }
+    }
+    end
   end
 
   def extract_tag_one_data(single_series)
@@ -24,29 +52,34 @@ class ExtractData
   end
 
   def get_tag_one_from_series(series)
-    series.dig("tags", "tag_one")
+    case series
+    in { tags: { tag_one: tag }}
+      tag
+    end
   end
 
   def get_count_from_series(series)
-    series.dig("values").map(&:last).max
-  end
-
-  def series
-    @_series ||= JSON.parse(@response).dig("results", 0, "series")
+    case series
+    in { values: values }
+      values.map(&:last).max
+    end
   end
 end
 
 data =
-  "{\"results\":[{\"statement_id\":0,\"series\":[
-{\"name\":\"SeriesOne\",\"tags\":{\"tag_one\":\"123\"},\"columns\":[\"time\",\"count\"],\"values\":[
-[\"2018-07-19T18:50:04.640075045Z\",1],
-[\"2018-07-19T18:50:05.640075045Z\",2],
-[\"2018-07-19T18:50:06.640075045Z\",3]
-]},
-{\"name\":\"SeriesTwo\",\"tags\":{\"tag_one\":\"456\"},\"columns\":[\"time\",\"count\"],\"values\":[[\"2018-07-19T18:50:04.640075045Z\",189]]}
+  "{\"results\":[{\"statement_id\":0,\"series\":
+[
+  {\"name\":\"SeriesOne\",\"tags\":{\"tag_name\":\"123\"},\"columns\":[\"time\",\"count\"],\"values\":[
+  [\"2018-07-19T18:50:04.640075045Z\",1],
+  [\"2018-07-19T18:50:05.640075045Z\",2],
+  [\"2018-07-19T18:50:06.640075045Z\",3]]
+  },
+  {\"name\":\"SeriesTwo\",\"tags\":{\"tag_name\":\"456\"},\"columns\":[\"time\",\"count\"],\"values\":[[\"2018-07-19T18:50:04.640075045Z\",189]
+  ]}
+]
 
-]}]}"
+}]}"
 
 result = ExtractData.call(data)
 p result
-p result == {"123"=>{:tag=>"123", :count=>189}, "456"=>{:tag=>"456", :count=>189}}
+p result == {"123"=>{:tag=>"123", :count=>3}, "456"=>{:tag=>"456", :count=>189}}
